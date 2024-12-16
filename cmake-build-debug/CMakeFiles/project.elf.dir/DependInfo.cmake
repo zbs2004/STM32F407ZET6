59bc74b4d42e6d39bdfa25d2a2ef4dc72334b625
zbs2004 <zbs2004gg@qqq.com>
2024-12-17 00:16:40 +0800

    123

---
+++ cmake-build-debug/CMakeFiles/project.elf.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Core/Startup/startup_stm32f407zetx.s" "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/cmake-build-debug/CMakeFiles/project.elf.dir/Core/Startup/startup_stm32f407zetx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Core/Inc"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Core/Src/main.c" "CMakeFiles/project.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/project.elf.dir/Core/Src/main.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/project.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/project.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Core/Src/stm32f4xx_it.c" "CMakeFiles/project.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/project.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Core/Src/syscalls.c" "CMakeFiles/project.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/project.elf.dir/Core/Src/syscalls.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Core/Src/sysmem.c" "CMakeFiles/project.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/project.elf.dir/Core/Src/sysmem.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Core/Src/system_stm32f4xx.c" "CMakeFiles/project.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/project.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "C:/Users/zbs20/Documents/Tencent Files/793262016/FileRecv/SEG/SEG/project/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/project.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
